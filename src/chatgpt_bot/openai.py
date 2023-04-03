0951c17be087c794a97da3ae7aa870cf2c074457
Luciano Scarpaci <tauxsik@gmail.com>
2023-04-02 23:23:40 -0400

    Update openai.py

--- src/chatgpt_bot/openai.py
+++ src/chatgpt_bot/openai.py
@@ -8,21 +8,26 @@ openai.api_key = os.getenv('CHATGPT_API_KEY')
 
 def chatgpt_response(prompt):
     # program needs to have prevention from a timeout to openAI
-    try:
-        response = openai.Completion.create(
-            # model type
-            model="text-davinci-003",
-            prompt=prompt,
-            temperature=1,
-            max_tokens=4000,
-        )
+    retry_count = 0
+    max_retries = 9999
 
-        response_dict = response.get("choices")
-        if response_dict and len(response_dict) > 0:
-            prompt_response = response_dict[0]["text"]
-        return prompt_response
+    while retry_count <= max_retries:
+        try:
+            # call openai api
+            response = openai.Completion.create(
+                # model type
+                model="text-davinci-003",
+                prompt=prompt,
+                temperature=1,
+                max_tokens=4000,
+            )
+            
+            response_dict = response.get("choices")
+            if response_dict and len(response_dict) > 0:
+                prompt_response = response_dict[0]["text"]
+            return prompt_response
     
-    except Exception:
-        print("An execption has occurred.")
+        except Exception:
+            retry_count += 1
 
         
