9f90b5e606eb9affea1112529d3a3a35e062c63e
Luciano Scarpaci <tauxsik@gmail.com>
2023-04-02 22:47:20 -0400

    Add exception handling for API

--- src/chatgpt_bot/openai.py
+++ src/chatgpt_bot/openai.py
@@ -7,15 +7,22 @@ load_dotenv()
 openai.api_key = os.getenv('CHATGPT_API_KEY')
 
 def chatgpt_response(prompt):
-    response = openai.Completion.create(
-        # model type
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=1,
-        max_tokens=4000,
-    )
+    # program needs to have prevention from a timeout to openAI
+    try:
+        response = openai.Completion.create(
+            # model type
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=1,
+            max_tokens=4000,
+        )
 
-    response_dict = response.get("choices")
-    if response_dict and len(response_dict) > 0:
-        prompt_response = response_dict[0]["text"]
-    return prompt_response
+        response_dict = response.get("choices")
+        if response_dict and len(response_dict) > 0:
+            prompt_response = response_dict[0]["text"]
+        return prompt_response
+    
+    except Exception:
+        print("An execption has occurred.")
+
+        
