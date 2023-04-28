c307b356b5002aa868b2916e16a7783018eb066f
Luciano Scarpaci <tauxsik@gmail.com>
2023-04-28 03:34:58 -0400

    Update voice commands

--- src/discord_bot/discordapi.py
+++ src/discord_bot/discordapi.py
@@ -18,17 +18,17 @@ class MyClient(discord.Client):
             return
         command, own_message=None, None
 
-        for text in ['/g','/bae']:
+        for text in ['Kinzie']:
             if message.content.startswith(text):
                 command=message.content.split(' ')[0]
                 own_message=message.content.replace(text, '')
                 print(command, own_message)
 
-        if command == '/g' or command == '/bae':
+        if command == 'Kinzie':
             bot_response = chatgpt_response(prompt=own_message)
             await message.channel.typing()
             await asyncio.sleep(1)
-            await message.channel.send(f"Hey boo {message.author.mention}, {bot_response}")
+            await message.channel.send(f"{bot_response}")
 
 intents = discord.Intents.default()
 intents.message_content = True
