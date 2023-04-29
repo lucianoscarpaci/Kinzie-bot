94952422c7eaf0258bcc7ce2bbdaef7e7854794d
Luciano Scarpaci <tauxsik@gmail.com>
2023-04-28 22:46:34 -0400

    Update discordapi.py

--- src/discord_bot/discordapi.py
+++ src/discord_bot/discordapi.py
@@ -2,15 +2,23 @@ from dotenv import load_dotenv
 import discord
 import os
 import asyncio
+import requests
+import json
 from src.chatgpt_bot.openai import chatgpt_response
 load_dotenv()
 
 
 discord_token = os.getenv('DISCORD_TOKEN')
+giphy_token = os.getenv('GIPHY_URL')
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print("login successful, you are: ", self.user)
+    
+    async def get_memes():
+        url = giphy_token
+        giphy_response = requests.get(url)
+        return json.loads(giphy_response.text)['data']['url']
     
     async def on_message(self, message):
         print(message.content)
@@ -29,6 +37,15 @@ class MyClient(discord.Client):
             await message.channel.typing()
             await asyncio.sleep(1)
             await message.channel.send(f"{bot_response}")
+        
+        if message.content.startswith('$meme'):
+            meme_url = get_memes()
+            await message.channel.send(meme_url)
+
+
+    
+    
+
 
 intents = discord.Intents.default()
 intents.message_content = True
