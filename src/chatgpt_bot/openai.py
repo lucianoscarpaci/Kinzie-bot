d5133141b4f911e0f5fecfbf9c47026826219ace
Luciano Scarpaci <tauxsik@gmail.com>
2023-05-29 22:29:28 -0400

    Final Exception Fix

--- src/chatgpt_bot/openai.py
+++ src/chatgpt_bot/openai.py
@@ -7,52 +7,37 @@ load_dotenv()
 
 openai.api_key = os.getenv('CHATGPT_API_KEY')
 
+
 def turbo_response(prompt):
-    
-    try:
-        # call openai api
-        response = openai.ChatCompletion.create(
-            # model type
-            model="gpt-3.5-turbo-0301",
-            messages=[
-                {"role": "user", "content": prompt},
-            ],
-            temperature=1,
-            max_tokens=4000,
-        )
 
-        response_dict = response.get("choices")
-        if response_dict and len(response_dict) > 0:
-            prompt_response = response_dict[0]["message"]["content"]
-        return prompt_response
+    # program needs to have prevention from a timeout to openAI
+    retry_count = 0
+    max_retries = 9999
 
-    except openai.error.Timeout as e:
-        print(f"API request failed with error: {e}")
-        pass
+    while retry_count <= max_retries:
+        try:
+            # call openai api
+            response = openai.ChatCompletion.create(
+                # model type
+                model="gpt-3.5-turbo-0301",
+                prompt=prompt,
+                temperature=1,
+                max_tokens=4000,
+            )
 
-    except openai.error.APIError as e:
-        print(f"API request failed with error: {e}")
-        pass
+            response_dict = response.get("choices")
+            if response_dict and len(response_dict) > 0:
+                prompt_response = response_dict[0]["message"]["content"]
+            return prompt_response
 
-    except openai.error.APIConnectionError as e:
-        print(f"API request failed with error: {e}")
-        pass
+        except openai.error.InvalidRequestError as e:
+            print(f"API request [InvalidRequestError] failed with error: {e}")
+            smiley = emoji.emojize(":smiling_face_with_smiling_eyes:")
+            return turbo_response(prompt=smiley)
 
-    except openai.error.InvalidRequestError as e:
-        print(f"API request failed with error: {e}")
-        pass
+        except Exception:
+            retry_count += 1
 
-    except openai.error.AuthenticationError as e:
-        print(f"API request failed with error: {e}")
-        pass
-
-    except openai.error.PermissionError as e:
-        print(f"API request failed with error: {e}")
-        pass
-
-    except openai.error.RateLimitError as e:
-        print(f"API request failed with error: {e}")
-        pass
 
 def chat_response(prompt):
 
