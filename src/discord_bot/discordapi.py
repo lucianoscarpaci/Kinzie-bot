a7c79ee5ddaab960f2c58ba83a29470afa231e12
Luciano Scarpaci <tauxsik@gmail.com>
2023-06-13 12:05:22 -0400

    Time Trigger Update

--- src/discord_bot/discordapi.py
+++ src/discord_bot/discordapi.py
@@ -39,7 +39,8 @@ class MyClient(discord.Client):
         # set target time to 9 AM 
         target_time = datetime.time(hour=9, minute=0, second=0, microsecond=0)
 
-
+        # good morning flag set to false
+        good_morning = False
 
         while True:
             target_date = datetime.datetime.now(tz=est_tz).replace(hour=target_time.hour,
@@ -47,32 +48,35 @@ class MyClient(discord.Client):
                                                                    second=target_time.second,
                                                                    microsecond=0)
 
-            # If target time has already passed for today, set target date to tomorrow's date with target time
             if datetime.datetime.now(tz=est_tz) >= target_date:
                 target_date += datetime.timedelta(days=1)
-                # Update flag to indicate action has been triggered
                 print(target_date)
+                good_morning = False
+                print("good_morning =", good_morning)
 
             # Wait until target time is reached
-            await asyncio.sleep((target_date - datetime.datetime.now(tz=est_tz)).total_seconds())
+            reached = asyncio.sleep((target_date - datetime.datetime.now(tz=est_tz)).total_seconds())
+            await reached
 
-            # trigger the action
-            # get the user and create a DM to user
-            user = await client.fetch_user(discord_user_id)
-            channel = await user.create_dm()
-            text_response = chat_response(prompt="Good morning!。")
-            kinzie_photos = []
-            for photo in all_kinzie_photos:
-                filename = os.path.join(photo_dir, photo)
-                kinzie_photos.append(filename)
-            random_photo = random.choice(kinzie_photos)
-            with open(random_photo, 'rb') as f:
-                file = discord.File(f)
-            await channel.send(file=file)
-            await channel.send(f"{text_response}")
-
-            # wait for 60 seconds before checking again
-            await asyncio.sleep(60)
+            if reached and not good_morning:
+                # trigger the action
+                # get the user and create a DM to user
+                user = await client.fetch_user(discord_user_id)
+                channel = await user.create_dm()
+                text_response = chat_response(prompt="Good morning!。")
+                kinzie_photos = []
+                for photo in all_kinzie_photos:
+                    filename = os.path.join(photo_dir, photo)
+                    kinzie_photos.append(filename)
+                random_photo = random.choice(kinzie_photos)
+                with open(random_photo, 'rb') as f:
+                    file = discord.File(f)
+                await channel.send(file=file)
+                await channel.send(f"{text_response}")
+                # good morning flag set to true
+                good_morning = True
+                # sleep for 60 seconds to prevent the task from running again
+                await asyncio.sleep(60)
 
     async def on_message(self, message):
         global emoji_mode, kaomoji_mode
